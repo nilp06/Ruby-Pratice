6e93e2cca94ee331def72d5812503284a15b4184
nilpatel <nil.p@simformsolutions.com>
2023-07-05 14:59:19 +0530

    feat: add second.rb

---
+++ second.rb
@@ -0,0 +1,30 @@
+# a = []
+# i = 0
+# while i < 10
+#   num = gets.chomp.to_i
+#   a.unshift(num)
+#   i += 1
+# end
+# puts 'Enter Number : '
+# num = gets.chomp.to_i
+# print 'yes' if a.include?(num)
+
+# a = []
+# i = 0
+# while i < 20
+#   num = gets.chomp.to_i
+#   a.unshift(num)
+#   i += 1
+# end
+# print
+
+# def digits_sum(start, stop)
+# 	answer = 0
+# 	(start..stop).to_a.each do |x|
+# 		while x > 0
+# 			answer = answer +  x%10
+# 			x = x/10
+# 		end
+# 	end
+#   answer
+# end
